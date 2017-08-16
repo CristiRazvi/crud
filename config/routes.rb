a7f7332a8a1459cb56a1406876951c8de784caec
Cristi Stefan <cristi.r.stefan@gmail.com>
2017-08-16 20:38:17 +0300

    inlocuim definirea rutelor cu o metoda rails de definire

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,13 @@ Rails.application.routes.draw do
   root 'books#index'
 
   # get 'books', to: 'books#index' # or
-  resources :books, only: [:index]
-  get 'books/new', to: 'books#new'
-  post 'books', to: 'books#create'
-  get 'books/:id', to: 'books#show', as: :book
-  get 'books/:id/edit', to: 'books#edit', as: :edit_book
-  put 'books/:id', to: 'books#update'
-  patch 'books/:id', to: 'books#update'
-  delete 'books/:id', to: 'books#destroy'
+  resources :books
+  # get 'books/new', to: 'books#new'
+  # post 'books', to: 'books#create'
+  # get 'books/:id', to: 'books#show', as: :book
+  # get 'books/:id/edit', to: 'books#edit', as: :edit_book
+  # put 'books/:id', to: 'books#update'
+  # patch 'books/:id', to: 'books#update'
+  # delete 'books/:id', to: 'books#destroy'
 
 end
